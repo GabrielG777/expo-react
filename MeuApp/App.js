53641b89fbf8c33b3b2d4caab1bbcabf8f0c0b0e
gaaaaaaabriel <gg405246@gmail.com>
2025-09-22 20:09:02 -0300

    aula - 3]

--- MeuApp/App.js
+++ MeuApp/App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+
+  function increment() {
+    return setNumber(number + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+      <Text style={styles.text}>
+        {number}
+      </Text>
+
+      <TouchableOpacity onPress={increment}>
+        <Text style={styles.text}>
+          Olá mundo
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text:{
+    backgroundColor: 'white',
+    padding: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # expo-react
+
+
+Aula 03 
